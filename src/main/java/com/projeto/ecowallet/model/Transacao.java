e8d4e787891aefa31064cb4274a77dec4787431c
alvin-silvaM <alvarosm159@gmail.com>
2026-05-23 23:44:21 -0300

    refactor: adiciona tipo e categoria a classe transacao

--- src/main/java/com/projeto/ecowallet/model/Transacao.java
+++ src/main/java/com/projeto/ecowallet/model/Transacao.java
@@ -6,6 +6,8 @@ public abstract class Transacao {
     private String descricao;
     private double valor;
     private LocalDate data;
+    private TipoTransacao tipo;
+    private Categoria categoria;
 
     public Transacao(String descricao, double valor, LocalDate data) {
         this.descricao = descricao;
