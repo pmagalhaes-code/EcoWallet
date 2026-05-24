0bf61b9c7b2b4dd37a6f1d696fc6c7af91019739
alvin-silvaM <alvarosm159@gmail.com>
2026-05-23 23:50:58 -0300

    refactor: reorganiza construtores da classe Receita

--- src/main/java/com/projeto/ecowallet/model/Receita.java
+++ src/main/java/com/projeto/ecowallet/model/Receita.java
@@ -4,8 +4,8 @@ import java.time.LocalDate;
 
 public class Receita extends Transacao {
 
-    public Receita(String descricao, double valor, LocalDate data){
-        super(descricao, valor, data);
+    public Receita(String descricao, double valor, LocalDate data, TipoTransacao tipo, Categoria categoria) {
+        super(descricao, valor, data, tipo, categoria);
     }
 
     @Override
