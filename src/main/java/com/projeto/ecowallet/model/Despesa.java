38400381dbae730344531cefc128bfbf2c7513ac
alvin-silvaM <alvarosm159@gmail.com>
2026-05-23 23:50:28 -0300

    refactor: reorganiza construtores da classe despesa

--- src/main/java/com/projeto/ecowallet/model/Despesa.java
+++ src/main/java/com/projeto/ecowallet/model/Despesa.java
@@ -3,8 +3,8 @@ package com.projeto.ecowallet.model;
 import java.time.LocalDate;
 
 public class Despesa extends Transacao {
-    public Despesa(String descricao, double valor, LocalDate data){
-        super(descricao, valor, data);
+    public Despesa(String descricao, double valor, LocalDate data, TipoTransacao tipo, Categoria categoria) {
+        super(descricao, valor, data, tipo, categoria);
     }
 
     @Override
